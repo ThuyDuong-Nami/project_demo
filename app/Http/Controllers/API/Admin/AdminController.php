f40ff022687f59011af3cd3bb539c1eb5c34b042
ThuyDuong-Nami <kieuthuy.duong@staff.gumiviet.com>
2021-05-20 08:40:26 +0700

    fix(admin): delete repo

--- app/Http/Controllers/API/Admin/AdminController.php
+++ app/Http/Controllers/API/Admin/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\AdminRequest;
 use App\Models\Admin;
-use App\Repositories\AdminRepository;
 use App\Transformers\Admin\AdminTransformer;
 use Illuminate\Support\Facades\Storage;
 
