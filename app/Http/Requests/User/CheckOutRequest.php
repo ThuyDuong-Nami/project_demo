003a6a307ede5d772847fbf4275e1ac7e0eb2af0
ThuyDuong-Nami <kieuthuy.duong@staff.gumiviet.com>
2021-06-02 15:23:23 +0700

    fix: checkout validate

--- app/Http/Requests/User/CheckOutRequest.php
+++ app/Http/Requests/User/CheckOutRequest.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Requests\User;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 
 class CheckOutRequest extends FormRequest
 {
@@ -29,5 +33,14 @@ class CheckOutRequest extends FormRequest
             'items' => 'required',
             'total' => 'required',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(
+            response()->json(['errors' => $errors], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
     }
 }
