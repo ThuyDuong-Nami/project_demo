d8136f8034fc520514a4ecd941d01c1c51a4179a
ThuyDuong-Nami <kieuthuy.duong@staff.gumiviet.com>
2021-06-03 14:18:34 +0700

    fix(admin): import product csv

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -2,6 +2,7 @@
 
 namespace App\Imports;
 
+use App\Models\Category;
 use App\Models\Product;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToModel;
@@ -13,24 +14,38 @@ class ProductsImport implements ToModel, WithHeadingRow, WithUpserts
     use Importable;
 
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         if ($product = Product::where('name', $row['name'])->first())
         {
             $product->categories()->detach();
             $product->update($row);
-            foreach (explode(',', $row['categories']) as $category){
-                $product->categories()->attach($category);
+            foreach (explode(',', str_replace(' ', '', $row['categories'])) as $category){
+                if ($categoryId = Category::where('name', $category)->first()){
+                    $product->categories()->attach($categoryId->id);
+                }else{
+                    $image = 'https://gumistore.s3-ap-southeast-1.amazonaws.com/categories/poster.jpg';
+                    $categoryArr = array_merge(['image' => $image], ['name' => $category]);
+                    $category = Category::create($categoryArr);
+                    $product->categories()->attach($category);
+                }
             }
             return $product;
         }else{
             $product = Product::create($row);
-            foreach (explode(',', $row['categories']) as $category){
-                $product->categories()->attach($category);
+            foreach (explode(',', str_replace(' ', '', $row['categories'])) as $category){
+                if ($categoryId = Category::where('name', $category)->first()){
+                    $product->categories()->attach($categoryId->id);
+                }else{
+                    $image = 'https://gumistore.s3-ap-southeast-1.amazonaws.com/categories/poster.jpg';
+                    $categoryArr = array_merge(['image' => $image], ['name' => $category]);
+                    $category = Category::create($categoryArr);
+                    $product->categories()->attach($category);
+                }
             }
             return $product;
         }
