0a13b5246cef457b682dd22dcdc08dec71098c0d
ThuyDuong-Nami <kieuthuy.duong@staff.gumiviet.com>
2021-06-09 17:48:09 +0700

    fix: export

--- app/Exports/BillsExport.php
+++ app/Exports/BillsExport.php
@@ -34,9 +34,10 @@ class BillsExport implements FromCollection, WithHeadings
     */
     public function collection()
     {
-        $bills = Bill::select('id', 'bill_code', DB::raw('TO_CHAR(created_at, DD/MM/YYYY)'),
+//        $bills = Bill::select('id', 'bill_code', DB::raw('DATE_FORMAT(created_at, "%d/%m/%Y")'),
+//            'user_id', 'address', 'phone','total')->where('status', BillStatus::delivered)->get();
+        $bills = Bill::select('id', 'bill_code', 'to_char(created_at, DD/MM/YYYY)',
             'user_id', 'address', 'phone','total')->where('status', BillStatus::delivered)->get();
-
         foreach ($bills as $bill){
             $arr = [];
             if (!$bill->user->username){
